1e01ca37abd2f2523189e594b74fdaf349c4371f
Miller-Alexey <45891758+Davidenko-Alexey@users.noreply.github.com>
2019-02-04 07:00:44 +1000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modules
+import { MaterialModule } from './modules/material/material.module';
+import { AppRoutingModule } from './modules/app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { AllProductsComponent } from './components/products/all-products/all-products.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		AllProductsComponent,
+		HomeComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		MaterialModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
