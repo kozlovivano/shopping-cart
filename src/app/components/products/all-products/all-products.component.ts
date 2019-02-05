fb47af744020921e72602965aeeb9b88125a7f1a
Miller-Alexey <45891758+Davidenko-Alexey@users.noreply.github.com>
2019-02-05 23:08:02 +1000

    responsive

--- src/app/components/products/all-products/all-products.component.ts
+++ src/app/components/products/all-products/all-products.component.ts
@@ -7,11 +7,16 @@ import { DataService } from '../../../services/data.service';
 })
 export class AllProductsComponent implements OnInit {
 
+	sideRow = 2;
+	mainCol = 3;
 	constructor(
         public data: DataService
     ) { }
 
 	ngOnInit() {
 	}
-
+	onResize(event){
+		this.sideRow = (window.innerWidth <= 768) ? 1 : 2;
+		this.mainCol = (window.innerWidth <= 768) ? 12 : 3;
+	}
 }
